cbad4334522e2b63fddea764c90a872964a14f8a
KachiMaxxxxx <kachimax0101@gmail.com>
2025-05-20 16:40:08 +0100

    retouched html and css, working on api js

--- js/api/kanbanAPI.js
+++ js/api/kanbanAPI.js
@@ -28,11 +28,85 @@ static save(data){
 }
 
 static getItems(columnId) {
-    const column = this.read().find(column => column.id === columnId);
+    const data = this.read();
+    const column = data.find(column => column.id === columnId);
     
     if (!column) {
         return [];
     }
+    
     return column.items;
 }
-} 
+
+static insertItem(columnId, content) {
+    const data = this.read();
+    const column = data.find(column => column.id === columnId);
+    if (!column) {
+        throw new Error(`Column with id ${columnId} not found`);  
+    }  
+    const item = {
+        id: Math.floor(Math.random() * 100), content
+    };
+
+    column.items.push(item);
+    this.save(data);
+
+     return item;
+    }
+
+ static updateItem(itemId, newProps) {
+        const data = this.read();
+
+        let item = null;
+        let currentColumn = null;
+
+        for (const column of data) {
+            const foundItem = column.items.find(item => item.id === itemId);
+            if (foundItem) {
+                item = foundItem;
+                currentColumn = column;
+                break;
+            }
+        }
+
+        if (!item) {
+            throw new Error(`Item with id ${itemId} not found`);
+        }
+        
+        // Update content if provided
+        item.content = newProps.content === undefined ? item.content : newProps.content;
+        /*
+
+        // Move item to a new column and position
+        if (newProps.columnId !== undefined && newProps.position !== undefined) {
+            const targetColumn = data.find(column => column.id === newProps.columnId);
+
+            if (!targetColumn) {
+                throw new Error(`Target column with id ${newProps.columnId} not found`);
+            }
+
+            // Remove from old column
+            currentColumn.items = currentColumn.items.filter(i => i.id !== itemId);
+
+            // Insert into target column at the specified position
+            targetColumn.items.splice(newProps.position, 0, item);
+        }
+ */
+        this.save(data);
+    }
+}
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
-import kanbanAPI from "./api/kanbanAPI.js";
+import KanbanAPI from "./api/kanbanAPI.js";
+/*  console.log(KanbanAPI.insertItem(2, "I am new"));*/
+KanbanAPI.insertItem(1, "I am new.");
+KanbanAPI.insertItem(2, "Secondly, I am improving.");
+KanbanAPI.insertItem(3, "Thirdly, I am balanced.");
+KanbanAPI.updateItem(81, { content: "I am updated." });
 
-console.log(kanbanAPI.getItems(1));
+
+
+
+
+
+
