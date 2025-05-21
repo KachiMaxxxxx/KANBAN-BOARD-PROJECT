9369f0f1085f56d66f39b3105f17f948e1da8a15
KachiMaxxxxx <kachimax0101@gmail.com>
2025-05-21 14:10:18 +0100

    implemented column js files and modified some functionalities

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
-import KanbanAPI from "./api/KanbanAPI.js";
- /*  console.log(KanbanAPI.insertItem(2, "I am new"));
- KanbanAPI.insertItem(1, "I am new.");
-KanbanAPI.insertItem(2, "Secondly, I am improving.");
-KanbanAPI.insertItem(3, "Thirdly, I am balanced."); 
-KanbanAPI.deleteItem(87);
- */
+ import Kanban from './ui/Kanban.js';
+    new Kanban(
+        document.querySelector(".kanban")
+    );
 
 
 
